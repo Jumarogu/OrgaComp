dcd63bb69e37b4b2de0de368a07b43f117b7b5bf
Juan Manuel Romero Guardado <jmromerog511@gmail.com>
2015-11-18 20:10:15 -0600

    Pruebas de sensores

--- LightSensorEjemplo/LightSensorEjemplo.ino
+++ LightSensorEjemplo/LightSensorEjemplo.ino
@@ -1,5 +1,8 @@
 int sensorValue = 0;
-int lightSensorPin = A0;
+int lightSensorPin0 = A0;
+int lightSensorPin1 = A1;
+int lightSensorPin2 = A3;
+int lightSensorPin3 = A4;
 
 void setup() {
   // put your setup code here, to run once:
@@ -8,8 +11,21 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  sensorValue = analogRead(lightSensorPin);
-  delay(1000);
-
+  sensorValue = analogRead(lightSensorPin0);
   Serial.println(sensorValue);
+  delay(800);
+  
+  sensorValue = analogRead(lightSensorPin1);
+  Serial.println(sensorValue);
+  delay(800);
+  
+  sensorValue = analogRead(lightSensorPin2);
+  Serial.println(sensorValue);
+  delay(800);
+  
+  sensorValue = analogRead(lightSensorPin3);
+  Serial.println(sensorValue);
+  delay(800);
+  
+  Serial.println("DONE");
 }
